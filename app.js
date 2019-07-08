fcba52c944792c8d987147429cc4b3493803ba7d
Sergio Heynes <sergio.heynes@gmail.com>
2019-07-08 09:18:23 -0500

    Added animation.

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ let randomDiceImage1 = document.getElementsByTagName('img')[0].setAttribute('src
 let randomDiceImage2 = document.getElementsByTagName('img')[1].setAttribute('src', 'images/dice' + randomNumber2 + '.png');
 
 let text = document.getElementsByTagName('h1')[0];
+let textPlayer1 = document.getElementsByTagName('p')[0];
+let textPlayer2 = document.getElementsByTagName('p')[1];
 
 console.log(text);
 
 if(randomNumber1 > randomNumber2) {
     text.innerHTML = "🚩 Player 1 Wins";
+    textPlayer1.classList.add("winner");
 } else if(randomNumber1 < randomNumber2 ) {
     text.innerHTML = "Player 2 Wins 🚩";
+    textPlayer2.classList.add("winner");
 } else {
     text.innerHTML = "It's a tie!";
 }
